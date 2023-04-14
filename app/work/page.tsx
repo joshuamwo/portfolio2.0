8dadc50d5f9a9e25996008bfc939c408da946e20
Yullu <joshuamuuo5@gmail.com>
2023-04-14 20:50:26 +0300

    +project thesafehouse

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,31 @@
+import siteMetadata from "../../data/siteMetaData";
+import projectsData from "../../data/projectsData";
+import Card from "../../components/Card";
+import { PageSEO } from "../../components/SEO";
+
 export default function Work() {
   return (
-    <section>
-      <h1 className="font-bold text-3xl font-serif">Work Showcase</h1>
-    </section>
+    <>
+      <PageSEO
+        title={`Projects - ${siteMetadata.author}`}
+        description={siteMetadata.description}
+      />
+      <div className="">
+        <h1 className="font-bold text-3xl font-serif mb-5">Work Showcase</h1>
+        <div className="container py-4">
+          <div className="-m-4 flex flex-wrap">
+            {projectsData.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
---
+++ data/projectsData.ts
@@ -0,0 +1,10 @@
+const projectsData: any = [
+  {
+    title: "TheeSafeHouse Website + Blog",
+    description: `The Safe House is a virtual space for Kenyan youths to discuss mental health and life.`,
+    imgSrc: "/images/tshpodcast.jpg",
+    href: "https://thesafehouse.vercel.app/",
+  },
+];
+
+export default projectsData;
