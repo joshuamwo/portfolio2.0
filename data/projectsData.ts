b16275747ac2aa20ba91d2bdc5f9601b7b3443f1
Yullu <joshuamuuo5@gmail.com>
2023-04-14 21:16:49 +0300

    +project : supabase auth ui

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,9 +1,15 @@
 const projectsData: any = [
   {
-    title: "TheeSafeHouse Website + Blog",
+    title: "TheSafeHouse Website + Blog",
     description: `The Safe House is a virtual space for Kenyan youths to discuss mental health and life.`,
     imgSrc: "/images/tshpodcast.jpg",
     href: "https://thesafehouse.vercel.app/",
+  },
+  {
+    title: "Supabase + Nextjs AuthUi",
+    description: `An authentication starter for Nextjs with Supabase. Easy to use and customize. See my github repo for more details.`,
+    imgSrc: "/images/authui.ppg",
+    href: "https://yullu.me/work/supabase-authui",
   },
 ];
 
