aab86219d5d664210a8574682e7bf32daf9e414e
Yullu <joshuamuuo5@gmail.com>
2023-04-14 13:12:48 +0300

    new about

--- lib/info.tsx
+++ lib/info.tsx
@@ -5,11 +5,9 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm a developer with an ambition to acquire more skills in the tech
-      world so I can offer solutions to the growing demands of technology
-      globally. I love to collaborate so hit me up at any of my socials. I
-      strongly believe that when people come together, they can achieve
-      anything.
+      Hey, I'm a developer who loves to collaborate so don't hesitate to hit me
+      up at any of my socials. I strongly believe that when people come
+      together, they can achieve anything.
     </>
   );
 };
