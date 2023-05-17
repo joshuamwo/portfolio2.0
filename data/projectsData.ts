8291a077fd81f7c072fe2cd64eb64615d030220c
YulluOne <38428314+joshuamwo@users.noreply.github.com>
2023-05-17 11:36:02 +0300

    Update projectsData.ts
    new projects: WeDance, Portfolio Starter, Netflix & Airbnb

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,4 +1,18 @@
 const projectsData: any = [
+{
+    title: "Dev Portfolio Starter",
+    description: `A stunning starter template for a Dev/Designer Portfolio.`,
+    imgSrc: "/images/portfolio-starter.png",
+    href: "https://yullu.me/work/dev-potfolio-starter",
+  },
+{
+    title: "WeDance",
+    description: `WeDance is a platform that connects people passionate about dance.`,
+    imgSrc: "/images/wedance.png",
+    href: "https://wedance.vip/",
+  },
+
+
   {
     title: "TheSafeHouse Website + Blog",
     description: `The Safe House is a virtual space for Kenyan youths to discuss mental health and life.`,
@@ -10,6 +24,18 @@ const projectsData: any = [
     description: `An authentication starter for Nextjs with Supabase. Easy to use and customize. See my github repo for more details.`,
     imgSrc: "/images/authui.png",
     href: "https://yullu.me/work/supabase-authui",
+  },
+{
+    title: "Netflix UI Rebuild",
+    description: `I rebuilt the neflix UI.`,
+    imgSrc: "/images/netflix-rebuild.png",
+    href: "https://yullu.me/work/netflix-rebuild",
+  },
+{
+    title: "AirBnB Rebuild",
+    description: `AirBnB UI re-imagined`,
+    imgSrc: "/images/airbnb-rebuild.png",
+    href: "https://yullu.me/work/airbnb-rebuild",
   },
 ];
 
