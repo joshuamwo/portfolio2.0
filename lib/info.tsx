8c6874911961e4d23cad7b6772dfa61960b94b1d
joshuamwo <joshuamuuo5@gmail.com>
2023-05-18 12:15:00 +0300

    new about & bio

--- lib/info.tsx
+++ lib/info.tsx
@@ -5,28 +5,52 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      <h3>I'm a Full-Stack Engineer with experience in both front-end and back-end development. I specialize in developing web applications using modern web technologies such as ReactJS, NodeJS, and VueJs</h3>
-      <h2>Services</h2>
-      <p> Custom Webstes | UI/UX Design | E-commerce Solutions | Website Maintenance and Support | Business Process Automation </p>
+      <h3 className="text-xl">
+        I'm a <b>Full-Stack Engineer</b> with experience in both{" "}
+        <b>front-end & back-end</b> development. I specialize in developing{" "}
+        <b>Web & Mobile</b> applications using modern web technologies such as{" "}
+        <b>ReactJS, Flutter, VueJs and NodeJs</b>
+      </h3>
+      <br />
+      <h2 className="text-3xl font-bold">Services</h2>
+
+      <p className="font-semibold">
+        <p> Custom Websites </p>
+        <p> UI/UX Design </p>
+        <p> Business Process Automation</p>
+        <p> Mobile App Development</p>
+        <p> E-commerce Solutions </p>
+        <p> Website Maintenance and Support</p>
+      </p>
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-<h3> > Languages </h2>
-<p>TypeScript | JavaScript (ES6) | Dart | Python | Rust | C++  </p>
-<h1> My Stack </h1>
-<h3> > Front End </h2>
-<p> React | Vue | NextJs | Nuxt | HTML5 | CSS  </p>
-<h3> > Back End </h2>
-<p> NodeJS | Express | GraphQL | Docker | Express | GraphQl </p>
-<h3> >Databases</h2>
-<p> DataBase Design | [SQL|NOSQL] | MongoDB | PostgreSQL | MySQL | FireStore </p>
-<h3> > DevOps </h2>
-<p> NodeJS | Express | GraphQL | Docker | </p>
-<h3> > DevOps & Cloud Tech </h2>
-<p> Git & Github | Docker | AWS | GCP | Firebase | Docker | Supabase | Vercel | Netlify | Heroku</p>     
+      <h1 className="text-2xl font-bold"> My Stack </h1>
+      <h3 className="text-xl font-bold"> Languages </h3>
+      <p>TypeScript | JavaScript (ES6) | Dart | Python | Rust | C++ </p>
+      <br />
+      <h3 className="text-xl font-bold">Front End </h3>
+      <p> React | Vue | Flutter | NextJs | Nuxt | HTML5 | CSS </p>
+      <br />
+      <h3 className="text-xl font-bold"> Back End </h3>
+      <p> NodeJS | Express | GraphQL | Docker | Express | GraphQl </p>
+      <br />
+      <h3 className="text-xl font-bold">Databases</h3>
+      <p>
+        DataBase Design | [SQL|NOSQL] | MongoDB | PostgreSQL | MySQL | FireStore{" "}
+      </p>
+      <br />
+      <h3 className="text-xl font-bold"> DevOps </h3>
+      <p> NodeJS | Express | GraphQL | Docker | </p>
+      <br />
+      <h3 className="text-xl font-bold">DevOps & Cloud Tech </h3>
+      <p>
+        Git & Github | Docker | AWS | GCP | Firebase | Docker | Supabase |
+        Vercel | Netlify | Heroku
+      </p>
     </>
   );
 };
