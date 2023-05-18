d861d1cc8903b21c3458d149442ba39ffbac9be5
YulluOne <38428314+joshuamwo@users.noreply.github.com>
2023-05-18 09:22:51 +0300

    Update info.tsx
    new landing

--- lib/info.tsx
+++ lib/info.tsx
@@ -5,19 +5,28 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      FullStack & DevOps Engineer || React | Vue | Node | Express | Next | Nuxt
-      | GraphQl | Git | Docker | WordPress || TypeScript | JavaScript (ES6) |
-      Dart | Python | Rust | C++ | HTML5 | CSS || Databases' Design |
-      [SQL|NOSQL] | MongoDB | PostgreSQL | MySQL | Firebase | Supabase | AWS |
-      GCP | Heroku | Vercel | Netlify ||
+      <h3>I'm a Full-Stack Engineer with experience in both front-end and back-end development. I specialize in developing web applications using modern web technologies such as ReactJS, NodeJS, and VueJs</h3>
+      <h2>Services</h2>
+      <p> Custom Webstes | UI/UX Design | E-commerce Solutions | Website Maintenance and Support | Business Process Automation </p>
     </>
   );
 };
 export const bio = () => {
   return (
     <>
-      When I'm not coding, I'm probably playing reading comics or watching
-      anime. I also love to visit new places and explore new expreriences.
+<h3> > Languages </h2>
+<p>TypeScript | JavaScript (ES6) | Dart | Python | Rust | C++  </p>
+<h1> My Stack </h1>
+<h3> > Front End </h2>
+<p> React | Vue | NextJs | Nuxt | HTML5 | CSS  </p>
+<h3> > Back End </h2>
+<p> NodeJS | Express | GraphQL | Docker | Express | GraphQl </p>
+<h3> >Databases</h2>
+<p> DataBase Design | [SQL|NOSQL] | MongoDB | PostgreSQL | MySQL | FireStore </p>
+<h3> > DevOps </h2>
+<p> NodeJS | Express | GraphQL | Docker | </p>
+<h3> > DevOps & Cloud Tech </h2>
+<p> Git & Github | Docker | AWS | GCP | Firebase | Docker | Supabase | Vercel | Netlify | Heroku</p>     
     </>
   );
 };
